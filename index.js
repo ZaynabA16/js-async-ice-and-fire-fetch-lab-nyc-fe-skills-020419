66f5fb063a53844d2f519f0af9e570b00c60bd0f
Zainab Agboola <zagboola@nyit.edu>
2019-04-03 22:37:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-  //write fetch request to the Game of Thrones API
+ fetch()
+ return booksData.json()
+}).then((booksJson) => console.log(booksJson))
 }
 
 function renderBooks(json) {
